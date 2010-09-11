a9221d5d11ba430eb1e738c7c2911479e35d203b
jerome <jerome>
2010-09-11 08:21:41 +0000

    auto update repository

--- build-package.sh
+++ build-package.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 python setup.py --command-packages=stdeb.command bdist_deb
 sudo dpkg -i deb_dist/python-imagizer_1.0-1*.deb
+scp deb_dist/python-imagizer_1.0-1*.deb islay:/home/httpd/html/devel
+scp deb_dist/imagizer_1.0-1.dsc islay:/home/httpd/html/devel
+scp deb_dist/imagizer_1.0.orig.tar.gz islay:/home/httpd/html/devel
+scp deb_dist/imagizer_1.0-1.diff.gz islay:/home/httpd/html/devel
+scp deb_dist/imagizer_1.0-1_amd64.changes islay:/home/httpd/html/devel
