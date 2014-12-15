7c9bbae4a098943ca3112742e4adcb5587448181
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2014-12-15 19:59:05 +0100

    update builder

--- build-package.sh
+++ build-package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 python setup.py --command-packages=stdeb.command bdist_deb
-sudo dpkg -i deb_dist/python-imagizer_1.*.deb
-scp deb_dist/python-imagizer_1.*.deb jerome@islay:/home/httpd/html/devel
-scp deb_dist/imagizer_1.*-1.dsc jerome@islay:/home/httpd/html/devel
-scp deb_dist/imagizer_1.*.orig.tar.gz jerome@islay:/home/httpd/html/devel
-scp deb_dist/imagizer_1.*-1.diff.gz jerome@islay:/home/httpd/html/devel
-scp deb_dist/imagizer_1.*-1_amd64.changes jerome@islay:/home/httpd/html/devel
+sudo dpkg -i deb_dist/python-imagizer_2.*.deb
+scp deb_dist/python-imagizer_2.*.deb jerome@islay:/home/httpd/html/devel
+scp deb_dist/imagizer_2.*-1.dsc jerome@islay:/home/httpd/html/devel
+scp deb_dist/imagizer_2.*.orig.tar.gz jerome@islay:/home/httpd/html/devel
+scp deb_dist/imagizer_2.*-1.diff.gz jerome@islay:/home/httpd/html/devel
+scp deb_dist/imagizer_2.*-1_amd64.changes jerome@islay:/home/httpd/html/devel
