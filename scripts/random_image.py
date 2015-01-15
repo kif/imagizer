531443016606fcbb48921a47850fb64440b92c67
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2015-01-15 21:40:23 +0100

    enforce size !

--- scripts/random_image.py
+++ scripts/random_image.py
@@ -28,7 +28,7 @@ Create a symbolic link to any file in the repository
 
 """
 __author__ = "Jérôme Kieffer"
-__date__ = "06/01/2015"
+__date__ = "15/01/2015"
 __licence__ = "GPLv2"
 
 import os, sys, logging, random, glob
@@ -46,6 +46,8 @@ def scan():
         l = len(config.DefaultRepository) + 1
     all_jpg = [i[l:] for i in glob.glob(os.path.join(config.DefaultRepository, "*", "*.jpg"))]
     logger.debug("Scanned directory %s and found %i images" % (config.DefaultRepository, len(all_jpg)))
+    if not all_jpg:
+        logger.error("No JPEG files in repository %s!!!" % config.DefaultRepository)
     return all_jpg
 
 
@@ -62,15 +64,15 @@ def create_link(linkName, lst=None):
         if not lst:
             lst = scan()
             random.shuffle(lst)
-        imFile = lst.pop()
-        photo = Photo(imFile, dontCache=True)
+        image_file = lst.pop()
+        photo = Photo(image_file, dontCache=True)
         data = photo.readExif()
         rate = data.get("rate", 0)
         # 0 for x=5, 0.5 for x=3 and 0.99 for x=0
         treshold = -0.01733333 * rate * rate + -0.11133333 * rate + 0.99
         found = (photo.pixelsX > photo.pixelsY) and (random.random() >= treshold)
         if found:
-            print("%s[%s] ---> %s" % (imFile, rate, linkName))
+            print("%s[%s] ---> %s" % (image_file, rate, linkName))
             os.symlink(photo.fn, linkName)
             break
 
