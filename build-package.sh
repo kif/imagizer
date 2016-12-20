a1b0a1cf000a32d36e0e99fd029ed9c1510aa7a8
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-12-20 12:22:53 +0100

    upgrade buil-package for qt5

--- build-package.sh
+++ build-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python setup.py --command-packages=stdeb.command bdist_deb
-sudo dpkg -i deb_dist/python-imagizer_3.*.deb
+sudo dpkg -i deb_dist/python-imagizer_4.*.deb
 scp deb_dist/python-imagizer_3.*.deb jerome@islay:/home/httpd/html/devel
 scp deb_dist/imagizer_3.*-1.dsc jerome@islay:/home/httpd/html/devel
 scp deb_dist/imagizer_3.*.orig.tar.gz jerome@islay:/home/httpd/html/devel
