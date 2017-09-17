ce663c978d17375fcb81ca626b47b91ff3bb1250
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2017-09-17 17:33:17 +0200

    increment version

--- build-package.sh
+++ build-package.sh
@@ -2,7 +2,7 @@
 python setup.py --command-packages=stdeb.command bdist_deb
 sudo dpkg -i deb_dist/python-imagizer_3.*.deb
 scp deb_dist/python-imagizer_3.*.deb jerome@islay:/home/httpd/html/devel
-scp deb_dist/imagizer_3.*-1.dsc jerome@islay:/home/httpd/html/devel
-scp deb_dist/imagizer_3.*.orig.tar.gz jerome@islay:/home/httpd/html/devel
-scp deb_dist/imagizer_3.*-1.diff.gz jerome@islay:/home/httpd/html/devel
-scp deb_dist/imagizer_3.*-1_amd64.changes jerome@islay:/home/httpd/html/devel
+scp deb_dist/imagizer_4.*-1.dsc jerome@islay:/home/httpd/html/devel
+scp deb_dist/imagizer_4.*.orig.tar.gz jerome@islay:/home/httpd/html/devel
+scp deb_dist/imagizer_4.*-1.diff.gz jerome@islay:/home/httpd/html/devel
+scp deb_dist/imagizer_4.*-1_amd64.changes jerome@islay:/home/httpd/html/devel
