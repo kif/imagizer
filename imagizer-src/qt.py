e99e86288d450f30d834649ad9fd6bde664c2717
Jerome Kieffer <jerome.kieffer@esrf.fr>
2019-01-13 10:32:18 +0100

    merging

--- imagizer-src/qt.py
+++ imagizer-src/qt.py
@@ -31,11 +31,12 @@ __author__ = "Jerome Kieffer"
 __contact__ = "imagizer@terre-adelie.org"
 __license__ = "GPLv3+"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "01/01/2016"
+__date__ = "31/12/2017"
 __status__ = "production"
 
 from ._qt import * # noqa
 # from ._utils import * # noqa
+_logger = logging.getLogger(__name__)
 
 transformations = (Qt.FastTransformation, Qt.SmoothTransformation)
 
